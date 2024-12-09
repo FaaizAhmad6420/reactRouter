24af1fed6957738f533c252fe60b86fbe6ea5a2c
FaaizAhmad6420 <faizahmad6420@gmail.com>
2024-12-09 20:30:03 +0500

    react reuter practice

---
+++ src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+export default function User() {
+    const {id} = useParams()
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>User: {id}</div>
+  )
+}
